8e7b61e5c545728c3a24554c94472945661e9d7a
laventin85 <32778449+laventin85@users.noreply.github.com>
2020-04-13 20:58:53 +0200

    Create ossn.fr.php
    French translation file to component Greetings

---
+++ locale/ossn.fr.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   (softlab24.com).ossn
+ * @author    OSSN Core Team <info@softlab24.com>
+ * @copyright (C) SOFTLAB24 LIMITED
+ * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ * Transalated in French by laventin85.fr
+ */
+
+$en = array(
+	'greetings:morning' => 'Bonjour !',
+	'greetings:afternoon' => 'Bonne après-midi !',
+	'greetings:evening' => 'Bonsoir !',
+	'greetings:night' => 'Bonne Nuit!',
+	
+);
+ossn_register_languages('fr', $en); 
